c06ff8a1605590cbac69ede292131a65a216c5cc
greymd <greengregson@gmail.com>
2019-06-19 23:14:14 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/greymd/ojichatrix
+
+go 1.12
+
+require (
+	github.com/greymd/ojichat v0.2.0
+	golang.org/x/crypto v0.0.0-20190611184440-5c40567a22f8
+)
---
+++ ojichatrix.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+	"unicode/utf8"
+
+	"github.com/greymd/ojichat/generator"
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+type singleLine struct {
+	flag    bool
+	cursor  int
+	message []rune
+}
+
+func createMessage(n int) []rune {
+	s := ""
+	for {
+		result, _ := generator.Start(generator.Config{EmojiNum: 3})
+		s += result
+		if utf8.RuneCountInString(s) > n {
+			return []rune(s)
+		}
+	}
+}
+
+func printLightGreen(y, x int, ch string) {
+	fmt.Printf("\033[%d;%dH\033[1;32m%s\033[0;0m", y, x, ch)
+}
+
+func printDarkGreen(y, x int, ch string) {
+	fmt.Printf("\033[%d;%dH\033[2;32m%s\033[0;0m", y, x, ch)
+}
+
+func printWhite(y, x int, ch string) {
+	fmt.Printf("\033[%d;%dH\033[1;37m%s\033[0;0H", y, x, ch)
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	width, height, err := terminal.GetSize(0)
+	if err != nil {
+		os.Exit(1)
+	}
+	width--
+	height--
+
+	// Make bold
+	fmt.Println("\033[1;40m")
+	// clear
+	fmt.Println("\033[2J")
+	// fmt.Printf("%v %v\n", width, height)
+	a := map[int]*singleLine{}
+	for i := 0; i < width; i++ {
+		a[i] = &singleLine{false, 0, createMessage(height)}
+	}
+	for {
+		(*a[rand.Intn(width)]).flag = true
+		for x := range a {
+			if !(*a[x]).flag {
+				continue
+			}
+			y := (*a[x]).cursor
+			(*a[x]).cursor++
+			ny := (*a[x]).cursor
+			ch := string((*a[x]).message[(*a[x]).cursor])
+			if rand.Intn(2) == 0 {
+				printLightGreen(y, x, ch)
+			} else {
+				printDarkGreen(y, x, ch)
+			}
+			printWhite(ny, x, ch)
+			if (*a[x]).cursor >= height {
+				(*a[x]).cursor = 0
+				(*a[x]).message = createMessage(height)
+			}
+		}
+		time.Sleep(50000000)
+	}
+}
